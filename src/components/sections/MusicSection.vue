a64d8d01a8cf62aaf836ba04b6c4e1c9306bccdf
3urobeat <tomke@goritz.org>
2026-06-14 16:28:32 +0200

    feat: Add Music section

--- src/components/sections/MusicSection.vue
+++ src/components/sections/MusicSection.vue
@@ -5,7 +5,7 @@
  * Created Date: 2026-05-23 17:31:22
  * Author: 3urobeat
  *
- * Last Modified: 2026-05-24 15:41:10
+ * Last Modified: 2026-06-14 13:44:31
  * Modified By: 3urobeat
  *
  * Copyright (c) 2026 3urobeat <https://github.com/3urobeat>
@@ -29,7 +29,104 @@
 
         </template>
 
+        <!-- Profile Links -->
+        <template v-slot:links>
+            <div class="flex h-fit flex-wrap gap-6">
+                <!-- 3urobeat -->
+                <div class="custom-external-anchor-multi">
+                    <span class="flex items-center gap-2.5 px-3 py-1 w-40 whitespace-nowrap">
+                        <PhUser class="size-6 shrink-0" />
+                        3urobeat
+                    </span>
+                    <a
+                        href="https://open.spotify.com/artist/6N9JhTJqOCBYHdZAvptJKS"
+                        target="_blank"
+                        class="flex items-center justify-center px-3 py-2 hover:text-accent transition-colors"
+                        aria-label="Spotify"
+                    >
+                        <PhSpotifyLogo class="size-6 shrink-0" />
+                    </a>
+                    <a
+                        href="https://music.apple.com/us/artist/3urobeat/1764177108"
+                        target="_blank"
+                        class="flex items-center justify-center px-3 py-2 hover:text-accent transition-colors"
+                        aria-label="Apple Music"
+                    >
+                        <PhAppleLogo class="size-6 shrink-0" />
+                    </a>
+                    <a
+                        href="https://soundcloud.com/3urobeat"
+                        target="_blank"
+                        class="flex items-center justify-center px-3 py-2 hover:text-accent transition-colors"
+                        aria-label="SoundCloud"
+                    >
+                        <PhSoundcloudLogo class="size-6 shrink-0" />
+                    </a>
+                </div>
+                <!-- Projekt 33 -->
+                <div class="custom-external-anchor-multi">
+                    <span class="flex items-center gap-2.5 px-3 py-1 w-40 whitespace-nowrap">
+                        <PhUser class="size-6 shrink-0" />
+                        Projekt 33
+                    </span>
+                    <a
+                        href=""
+                        target="_blank"
+                        class="flex items-center justify-center px-3 py-2 hover:text-accent transition-colors"
+                        aria-label="Spotify"
+                    >
+                        <PhSpotifyLogo class="size-6 shrink-0" />
+                    </a>
+                    <a
+                        href=""
+                        target="_blank"
+                        class="flex items-center justify-center px-3 py-2 hover:text-accent transition-colors"
+                        aria-label="Apple Music"
+                    >
+                        <PhAppleLogo class="size-6 shrink-0" />
+                    </a>
+                    <a
+                        href="https://soundcloud.com/projekt-33"
+                        target="_blank"
+                        class="flex items-center justify-center px-3 py-2 hover:text-accent transition-colors"
+                        aria-label="SoundCloud"
+                    >
+                        <PhSoundcloudLogo class="size-6 shrink-0" />
+                    </a>
+                </div>
+            </div>
+        </template>
+
         <template v-slot:content>
+
+            <!-- Introduction -->
+            <p>
+                When not working on Software, I'm producing music from my home studio.
+            </p>
+            <p class="mt-2">
+                <span>
+                    From the
+                    <span class="text-text-variable transition-[--count-animation] duration-[2s] ease-out [counter-set:num_var(--count-animation)] supports-counter-set:before:content-[counter(num)] intersect-once intersect:[--count-animation:var(--target)]" no-intersect :style="{ '--target': tracks3urobeatAmount }">
+                        <span class="supports-counter-set:sr-only">{{ tracks3urobeatAmount }}</span> Hardstyle Tracks
+                    </span>
+                    released under
+                    <span class="text-text-variable">3urobeat</span>
+                    <br>
+                    and
+                    <span class="text-text-variable transition-[--count-animation] duration-[2s] ease-out [counter-set:num_var(--count-animation)] supports-counter-set:before:content-[counter(num)] intersect-once intersect:[--count-animation:var(--target)]" no-intersect :style="{ '--target': tracksP33Amount }">
+                        <span class="supports-counter-set:sr-only">{{ tracksP33Amount }}</span> House Tracks
+                    </span>
+                    published under my alias
+                    <span class="text-text-variable -mr-[1ch]">Projekt 33</span>
+                    ,
+                </span>
+            </p>
+            <span>you might want to check out:</span>
+
+            <!-- Notable Projects Accordion -->
+            <div class="pt-4">
+                <Accordion :cards="accordionCards"></Accordion>
+            </div>
 
         </template>
 
@@ -39,6 +136,43 @@
 
 
 <script setup lang="ts">
+    import { ref } from "vue";
+    import { PhAppleLogo, PhSoundcloudLogo, PhSpotifyLogo, PhUser } from "@phosphor-icons/vue";
     import SectionContainer from "../SectionContainer.vue";
+    import Accordion from "../Accordion.vue";
+    import { accordionColors, AccordionIcon, type AccordionCard } from "../../types/accordion";
+
+
+    const tracks3urobeatAmount = ref(7);
+    const tracksP33Amount      = ref(5);
+
+
+    // Elements displayed in notable projects accordion
+    const accordionCards: AccordionCard[] = [
+        {
+            title: "3urobeat - Come Take My Hand",
+            description: "2026",
+            color: accordionColors.amber,
+            icon: AccordionIcon.MUSIC_NOTES_SIMPLE,
+            bgImgUrl: "https://i.scdn.co/image/ab67616d0000b273120fc1d5e4f1c3945d5996fe",
+            clickUrl: "https://open.spotify.com/album/5ZZuT3fX3VKxdjss7cTSWd"
+        },
+        {
+            title: "3urobeat - Space & Time",
+            description: "2025",
+            color: accordionColors.blue,
+            icon: AccordionIcon.MUSIC_NOTES_SIMPLE,
+            bgImgUrl: "https://i.scdn.co/image/ab67616d0000b273a72c334cf52aca9a23873d12",
+            clickUrl: "https://open.spotify.com/track/0DixKXVB9RUCIA8J0rVEvU"
+        },
+        {
+            title: "3urobeat - Old Sounds, New Style",
+            description: "2025",
+            color: accordionColors.zinc,
+            icon: AccordionIcon.MUSIC_NOTES_SIMPLE,
+            bgImgUrl: "https://i.scdn.co/image/ab67616d0000b273a50ec0c67a61a5dbd6953e71",
+            clickUrl: "https://open.spotify.com/track/5ONb93fAyJwcRMG51s8v23"
+        }
+    ];
 
 </script>
