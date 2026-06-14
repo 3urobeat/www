1709eacc8cfb0a669ab07f2a9cdf9a7e14ba17e5
3urobeat <tomke@goritz.org>
2026-06-14 19:24:53 +0200

    feat: Add noscript warning

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -5,7 +5,7 @@
  * Created Date: 2026-05-25 22:28:19
  * Author: 3urobeat
  *
- * Last Modified: 2026-06-14 17:48:43
+ * Last Modified: 2026-06-14 19:19:39
  * Modified By: 3urobeat
  *
  * Copyright (c) 2026 3urobeat <https://github.com/3urobeat>
@@ -20,42 +20,48 @@
 <template>
 
     <!-- TODO: Pill Shape -->
-    <div class="fixed flex gap-4 right-4 top-4 z-30 text-bg-light">
+    <div class="fixed flex flex-col-reverse md:flex-row gap-4 right-0 px-4 top-4 z-30 text-bg-light">
 
-        <!-- Contact -->
-        <a
-            href="mailto:contact@goritz.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="flex items-center rounded-xl shadow-md hover:shadow-lg custom-glass-pill border-2! border-green-700! hover:border-green-500! hover:bg-green-500/30! gap-3 px-3 py-1.5 justify-center transition-all duration-300"
-            aria-label="Get in touch (opens in new tab)"
-        >
-            <PhEnvelopeOpen class="size-5.5 shrink-0" aria-hidden="true" />
-            <div>
-                <p>Get in touch!</p>
-            </div>
-        </a>
+        <noscript>
+            <p class="text-red-500 border-red-600! items-center rounded-xl shadow-md custom-glass-pill gap-3 px-3 py-1.5">JavaScript is disabled! Functionality is limited.</p>
+        </noscript>
 
-        <!-- Pill -->
-        <div class="custom-glass-pill p-1 rounded-2xl flex items-center justify-center divide-border-secondary-light dark:divide-border-secondary-dark divide-x">
+        <div class="flex gap-4 right-0 self-end">
+            <!-- Contact -->
             <a
-                class="flex px-1.5 py-0.5 hover:text-bg-input-hover-light transition-all"
-                href="/linktree"
-                aria-label="Linktree page"
+                href="mailto:contact@goritz.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="flex items-center rounded-xl shadow-md hover:shadow-lg custom-glass-pill border-2! border-green-700! hover:border-green-500! hover:bg-green-500/30! gap-3 px-3 py-1.5 justify-center transition-all duration-300"
+                aria-label="Get in touch (opens in new tab)"
             >
-                <PhLink class="size-5.5" aria-hidden="true" />
+                <PhEnvelopeOpen class="size-5.5 shrink-0" aria-hidden="true" />
+                <div>
+                    <p class="text-nowrap">Get in touch!</p>
+                </div>
             </a>
 
-            <!-- Light/Dark Mode toggle -->
-            <button
-                class="w-9 px-1.5 py-0.5 hover:text-bg-input-hover-light transition-all"
-                @click="toggleDarkMode()"
-                :aria-label="`Switch to ${isDark ? 'light' : 'dark'} mode`"
-                :aria-pressed="isDark"
-            >
-                <PhMoon class="hidden dark:block size-5.5" aria-hidden="true" />
-                <PhSun class="block dark:hidden size-5.5" aria-hidden="true" />
-            </button>
+            <!-- Pill -->
+            <div class="custom-glass-pill p-1 rounded-2xl flex items-center justify-center divide-border-secondary-light dark:divide-border-secondary-dark divide-x">
+                <a
+                    class="flex px-1.5 py-0.5 hover:text-bg-input-hover-light transition-all"
+                    href="/linktree"
+                    aria-label="Linktree page"
+                >
+                    <PhLink class="size-5.5" aria-hidden="true" />
+                </a>
+
+                <!-- Light/Dark Mode toggle -->
+                <button
+                    class="w-9 px-1.5 py-0.5 hover:text-bg-input-hover-light transition-all"
+                    @click="toggleDarkMode()"
+                    :aria-label="`Switch to ${isDark ? 'light' : 'dark'} mode`"
+                    :aria-pressed="isDark"
+                >
+                    <PhMoon class="hidden dark:block size-5.5" aria-hidden="true" />
+                    <PhSun class="block dark:hidden size-5.5" aria-hidden="true" />
+                </button>
+            </div>
         </div>
 
     </div>
